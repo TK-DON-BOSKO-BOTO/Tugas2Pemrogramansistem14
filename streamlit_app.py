73b89ba76a978a3400c439d08558f33e59873d97
TK DON BOSKO BOTO <161457774+TK-DON-BOSKO-BOTO@users.noreply.github.com>
2024-11-07 05:06:39 +0000

    ulang10

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ import tensorflow as tf
 from sklearn.preprocessing import StandardScaler
 
 # Muat model
+model = tf.keras.models.load_model('spam_classifier_model.h5')
 model = tf.keras.models.load_model('spam_classifier_model.h5', custom_objects={'custom_object_name': custom_object})
 
 # Inisialisasi scaler
